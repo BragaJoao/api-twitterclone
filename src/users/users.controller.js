387b8ba49953cbdb563af6313be51ae64fdb574a
BragaJoao <joaogbraga@hotmail.com.br>
2022-07-06 17:09:13 -0300

    find all users

--- src/users/users.controller.js
+++ src/users/users.controller.js
@@ -17,20 +17,27 @@ const createUserController = async (req, res) => {
       message: "Usuário já existe ",
     });
   }
-    const user = await userService
-      .createUserService(req.body)
-      .catch((err) => console.log(err.message));
+  const user = await userService
+    .createUserService(req.body)
+    .catch((err) => console.log(err.message));
 
-    if (!user) {
-      return res.status(400).send({
-        message: "Erro ao criar o usuário!",
-      });
-    }
+  if (!user) {
+    return res.status(400).send({
+      message: "Erro ao criar o usuário!",
+    });
+  }
 
-    res.status(201).send(user);
+  res.status(201).send(user);
 };
 const findAllUserController = async (req, res) => {
-  res.send({ message: "FindAll OK" });
+  const users = await userService.findAllUserService();
+
+  if (users.lenght === 0) {
+    return res.status(400).send({
+      message: "Não existem usários cadastrados",
+    });
+  }
+  res.send(users);
 };
 
 module.exports = {
